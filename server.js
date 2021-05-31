af5b7b0dccd18065233e631490a8c9464ca47a57
RaheelDiginativesIO <raheel.asghar@diginatives.io>
2021-05-31 17:44:02 +0500

    comment out the code from server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({msg: 'Welcome to the ContactKeeper API'}));
+// app.get('/', (req, res) => res.json({msg: 'Welcome to the ContactKeeper API'}));
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
